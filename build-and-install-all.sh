831a67cd0cdbec355330c3c07ee6a35b4fba79f4
John Axel Eriksson <john@insane.se>
2018-02-26 20:31:37 +0100

    More dynamic patching

--- build-and-install-all.sh
+++ build-and-install-all.sh
@@ -1,13 +1,30 @@
 #!/bin/sh
 
 PREFIX=$1
+BASEPIXELSIZE=${2:-16}
+
 if [ -z "$PREFIX" ]; then
     echo "Please supply the prefix where st should be installed"
     exit 1
 fi
 
-COLORS=dark ./p && make && cp st $PREFIX/bin/st
-COLORS=light ./p && make && cp st $PREFIX/bin/st-light
+if [ "$BASEPIXELSIZE" != "" ]; then
+    HIDPIPIXELSIZE=$(( $(( $BASEPIXELSIZE - 4 )) * 2 ))
+fi
 
-HIDPI=y COLORS=dark ./p && make && cp st $PREFIX/bin/st-hidpi
-HIDPI=y COLORS=light ./p && make && cp st $PREFIX/bin/st-hidpi-light
+echo "Base pixel size: $BASEPIXELSIZE"
+echo "HIDPI pixel size: $HIDPIPIXELSIZE"
+echo "Ok? (y/n)"
+
+read answer
+
+if ! echo "$answer" | grep -E '^y.*' 2>&1 > /dev/null; then
+    echo "ok then, bye"
+    exit 1
+fi
+
+PIXELSIZE=$BASEPIXELSIZE COLORS=dark ./p && make && cp st $PREFIX/bin/st
+PIXELSIZE=$BASEPIXELSIZE COLORS=light ./p && make && cp st $PREFIX/bin/st-light
+
+PIXELSIZE=$HIDPIPIXELSIZE COLORS=dark ./p && make && cp st $PREFIX/bin/st-hidpi
+PIXELSIZE=$HIDPIPIXELSIZE COLORS=light ./p && make && cp st $PREFIX/bin/st-hidpi-light
--- p
+++ p
@@ -1,21 +1,15 @@
 #!/usr/bin/sh
 
-SKIP="st-customized-hidpi.diff"
-if [ ! -z "$HIDPI" ]; then
-    SKIP="st-customized.diff"
-fi
-
-
-COLORS=${COLORS:-dark}
-
 make clean
 git reset HEAD --hard
 
+COLORS=${COLORS:-dark}
+
+if [ ! -z "$PIXELSIZE" ]; then
+    sed -i "s|Pro:pixelsize\=[0-9]*|Pro:pixelsize=$PIXELSIZE|g" st-customized.diff
+fi
+
 for patch in $(ls | grep '^st-.*\.diff'); do
-  if [ "$patch" = "$SKIP" ]; then
-      echo "SKIPPING patch: $patch"
-      continue
-  fi
   if echo $patch | grep "solarized" 2>&1 > /dev/null; then
       if ! echo $patch | grep "$COLORS" 2>&1 > /dev/null; then
           echo "SKIPPING patch: $patch"
